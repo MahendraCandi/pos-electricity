3d1881290f51be674d61cfe6a44cd865ffb5a2e4
candimahendra <abdumaadjidcandi@gmail.com>
2024-01-09 01:06:11 +0700

    update kode_tarif to unique on table tarif_listrik

--- dbdump/db-dumb.sql
+++ dbdump/db-dumb.sql
@@ -23,14 +23,14 @@ DROP TABLE IF EXISTS `pelanggan`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `pelanggan` (
-                             `pelanggan_id` int NOT NULL AUTO_INCREMENT COMMENT 'The customers ID''s',
-                             `tarif_listrik_id` int NOT NULL,
-                             `no_meter` varchar(15) NOT NULL COMMENT 'The customer''s number meter. Example: 12238888, 12334445, etc.',
-                             `nama` varchar(150) NOT NULL,
-                             `alamat` varchar(255) NOT NULL,
-                             PRIMARY KEY (`pelanggan_id`),
-                             KEY `pelanggan_tarif_listrik_tarif_listrik_id_fk` (`tarif_listrik_id`),
-                             CONSTRAINT `pelanggan_tarif_listrik_tarif_listrik_id_fk` FOREIGN KEY (`tarif_listrik_id`) REFERENCES `tarif_listrik` (`tarif_listrik_id`)
+  `pelanggan_id` int NOT NULL AUTO_INCREMENT COMMENT 'The customers ID''s',
+  `tarif_listrik_id` int NOT NULL,
+  `no_meter` varchar(15) NOT NULL COMMENT 'The customer''s number meter. Example: 12238888, 12334445, etc.',
+  `nama` varchar(150) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  PRIMARY KEY (`pelanggan_id`),
+  KEY `pelanggan_tarif_listrik_tarif_listrik_id_fk` (`tarif_listrik_id`),
+  CONSTRAINT `pelanggan_tarif_listrik_tarif_listrik_id_fk` FOREIGN KEY (`tarif_listrik_id`) REFERENCES `tarif_listrik` (`tarif_listrik_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='The customers';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -51,14 +51,14 @@ DROP TABLE IF EXISTS `tagihan`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tagihan` (
-                           `tagihan_id` int NOT NULL AUTO_INCREMENT COMMENT 'The tagihan ID''s',
-                           `pelanggan_id` int NOT NULL COMMENT 'The customer ID''s',
-                           `tahun_tagihan` varchar(4) NOT NULL COMMENT 'The year of invoice. Example: 2023, 2022, etc.',
-                           `bulan_tagihan` varchar(2) NOT NULL COMMENT 'The monthly invoice. Example: 01 (January), 02, (February), etc.',
-                           `pemakaian` decimal(10,0) NOT NULL COMMENT 'The total invoice. Exmple: 1000000, 500000, 100000, etc.',
-                           PRIMARY KEY (`tagihan_id`),
-                           KEY `tagihan_pelanggan_pelanggan_id_fk` (`pelanggan_id`),
-                           CONSTRAINT `tagihan_pelanggan_pelanggan_id_fk` FOREIGN KEY (`pelanggan_id`) REFERENCES `pelanggan` (`pelanggan_id`)
+  `tagihan_id` int NOT NULL AUTO_INCREMENT COMMENT 'The tagihan ID''s',
+  `pelanggan_id` int NOT NULL COMMENT 'The customer ID''s',
+  `tahun_tagihan` varchar(4) NOT NULL COMMENT 'The year of invoice. Example: 2023, 2022, etc.',
+  `bulan_tagihan` varchar(2) NOT NULL COMMENT 'The monthly invoice. Example: 01 (January), 02, (February), etc.',
+  `pemakaian` decimal(10,0) NOT NULL COMMENT 'The total invoice. Exmple: 1000000, 500000, 100000, etc.',
+  PRIMARY KEY (`tagihan_id`),
+  KEY `tagihan_pelanggan_pelanggan_id_fk` (`pelanggan_id`),
+  CONSTRAINT `tagihan_pelanggan_pelanggan_id_fk` FOREIGN KEY (`pelanggan_id`) REFERENCES `pelanggan` (`pelanggan_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='The customer''s invoice';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -79,15 +79,16 @@ DROP TABLE IF EXISTS `tarif_listrik`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tarif_listrik` (
-                                 `tarif_listrik_id` int NOT NULL AUTO_INCREMENT COMMENT 'The Tarif Listrik ID''s',
-                                 `user_id` int NOT NULL COMMENT 'The user ID''s',
-                                 `kode_tarif` varchar(4) NOT NULL COMMENT 'Example value: R101, R102, R103, R201, etc.',
-                                 `beban` decimal(10,0) NOT NULL COMMENT 'The ''Daya'' in VA. Example: 900, 1300, 2200, etc.',
-                                 `tarif_perkwh` double NOT NULL COMMENT 'The bill per kWh. Example: 1352, 1444, etc.',
-                                 PRIMARY KEY (`tarif_listrik_id`),
-                                 KEY `tarif_listrik_user_user_id_fk` (`user_id`),
-                                 CONSTRAINT `tarif_listrik_user_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `tarif_listrik_id` int NOT NULL AUTO_INCREMENT COMMENT 'The Tarif Listrik ID''s',
+  `kode_tarif` varchar(4) NOT NULL COMMENT 'Example value: R101, R102, R103, R201, etc.',
+  `beban` decimal(10,0) NOT NULL COMMENT 'The ''Daya'' in VA. Example: 900, 1300, 2200, etc.',
+  `tarif_perkwh` decimal(10,0) NOT NULL COMMENT 'The bill per kWh. Example: 1352, 1444, etc.',
+  `created_by` int NOT NULL COMMENT 'The created by User ID',
+  `created_date` datetime NOT NULL COMMENT 'Date time created date',
+  `updated_date` datetime DEFAULT NULL COMMENT 'The date time updated date',
+  PRIMARY KEY (`tarif_listrik_id`),
+  UNIQUE KEY `kode_tarif` (`kode_tarif`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -96,6 +97,7 @@ CREATE TABLE `tarif_listrik` (
 
 LOCK TABLES `tarif_listrik` WRITE;
 /*!40000 ALTER TABLE `tarif_listrik` DISABLE KEYS */;
+INSERT INTO `tarif_listrik` VALUES (1,'R101',900,1500,2,'2024-01-09 00:25:42',NULL),(2,'R102',1300,1600,3,'2024-01-09 00:26:54',NULL),(3,'R103',2200,1700,3,'2024-01-09 00:56:39',NULL);
 /*!40000 ALTER TABLE `tarif_listrik` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -107,13 +109,13 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-                        `user_id` int NOT NULL AUTO_INCREMENT COMMENT 'The user ID''s',
-                        `username` varchar(12) NOT NULL,
-                        `password` text NOT NULL COMMENT 'Hashed password',
-                        `hak_akses` int NOT NULL COMMENT 'The access right. 1. ADMIN 2. SALES',
-                        `created_date` datetime DEFAULT NULL,
-                        PRIMARY KEY (`user_id`),
-                        UNIQUE KEY `username_unique` (`username`)
+  `user_id` int NOT NULL AUTO_INCREMENT COMMENT 'The user ID''s',
+  `username` varchar(12) NOT NULL,
+  `password` text NOT NULL COMMENT 'Hashed password',
+  `hak_akses` int NOT NULL COMMENT 'The access right. 1. ADMIN 2. SALES',
+  `created_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username_unique` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='All the user has access to the app';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -136,4 +138,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-01-08 22:50:23
+-- Dump completed on 2024-01-09  1:04:54
